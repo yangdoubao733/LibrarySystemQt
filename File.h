ed2da45e16f81fbb783893b64213424305b3df2a
yf s <2475939564@qq.com>
2025-06-13 22:37:32 +0800

    修改了文件相关操作

--- File.h
+++ File.h
@@ -10,9 +10,9 @@ bookList readBookFile(const char* fileName);//读取文件返回链表头指针
 void writeBookFile(const char* fileName, bookList head);//将链表写入文件（存储时id从1开始递增）
 
 userList readUserFile(const char* fileName);//读取文件返回链表头指针
-void writeUserFile(const char* fileName, bookList head);//将链表写入文件（存储时id从1开始递增）
+void writeUserFile(const char* fileName, userList head);//将链表写入文件（存储时id从1开始递增）
 
 adminList readAdminFile(const char* fileName);//读取文件返回链表头指针
-void writeAdminFile(const char* fileName, bookList head);//将链表写入文件（存储时id从1开始递增）
+void writeAdminFile(const char* fileName, List adminhead);//将链表写入文件（存储时id从1开始递增）
 
 #endif // !BOOKFILE
