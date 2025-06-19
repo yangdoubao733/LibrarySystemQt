8ecd14247b1ead1813f282f3bec2c000e6ba09cb
C-luminous <24211128@bjtu.edu.cn>
2025-06-19 23:59:23 +0800

    修改了文件的部分代码

--- File.h
+++ File.h
@@ -12,4 +12,6 @@ void writeBookFile(const char* fileName, bookList head);//将链表写入文件（存储时
 userList readUserFile(const char* fileName);//读取文件返回链表头指针
 void writeUserFile(const char* fileName, userList head);//将链表写入文件（存储时id从1开始递增）
 
+adminList readAdminFile(const char* fileName);// 读取管理员文件，返回带头节点的管理员链表
+
 #endif // !BOOKFILE
